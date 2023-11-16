7e27be58e9d510e56f6e3d840d0ff3b2d5473119
Cardinal Alpha <renaldi96.aldi@gmail.com>
2023-11-16 13:09:59 +0700

    Start Spring project

---
+++ src/test/java/ist/challenge/renaldi/RenaldiApplicationTests.java
@@ -0,0 +1,13 @@
+package ist.challenge.renaldi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenaldiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
