e595a9154863c7177f010cfdc147fb827e4edb35
Cardinal Alpha <renaldi96.aldi@gmail.com>
2023-11-16 13:36:53 +0700

    Add user registration POJO

---
+++ src/main/java/ist/challenge/renaldi/pojo/UserRegistration.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ist.challenge.renaldi.pojo;
+
+import lombok.Data;
+
+/**
+ *
+ * @author ryuuji
+ */
+@Data
+public class UserRegistration {
+    
+    private String username, password;
+    
+}
