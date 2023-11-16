c6ee98c3c7dfb4a09eaed0547312036776034c98
Cardinal Alpha <renaldi96.aldi@gmail.com>
2023-11-16 15:33:39 +0700

    Add user database repository

---
+++ src/main/java/ist/challenge/renaldi/repository/UserRepository.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package ist.challenge.renaldi.repository;
+
+import ist.challenge.renaldi.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author ryuuji
+ */
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+    
+}
