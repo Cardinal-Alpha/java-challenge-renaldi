d205da0aba11abbdcdf22d7a7b86da20556ddb52
Cardinal Alpha <renaldi96.aldi@gmail.com>
2023-11-16 17:10:25 +0700

    Add query to check if user already exist

--- src/main/java/ist/challenge/renaldi/repository/UserRepository.java
+++ src/main/java/ist/challenge/renaldi/repository/UserRepository.java
@@ -15,4 +15,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
     
+    boolean existsByUsername(String username);
+    
 }
