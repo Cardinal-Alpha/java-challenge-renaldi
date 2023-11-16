ca449d3740e2355342e068f9ad0604f7e0fcdec6
Cardinal Alpha <renaldi96.aldi@gmail.com>
2023-11-16 17:13:29 +0700

    Exception handling for duplicate user registration

---
+++ src/main/java/ist/challenge/renaldi/controller/advice/UserExceptionAdvice.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ist.challenge.renaldi.controller.advice;
+
+import ist.challenge.renaldi.exception.user.UserAlreadyExistException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+/**
+ *
+ * @author ryuuji
+ */
+@ControllerAdvice
+public class UserExceptionAdvice {
+    
+    @ExceptionHandler
+    public ResponseEntity<String> handleDataException(UserAlreadyExistException ex){
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+                                .body("Username sudah terpakai");
+    }
+    
+}
