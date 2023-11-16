48cc2993e4e0b9c2e182e687277569766413bc83
Cardinal Alpha <renaldi96.aldi@gmail.com>
2023-11-16 17:11:56 +0700

    Throw and exception upon username conflict

--- src/main/java/ist/challenge/renaldi/service/UserService.java
+++ src/main/java/ist/challenge/renaldi/service/UserService.java
@@ -5,6 +5,7 @@
 package ist.challenge.renaldi.service;
 
 import ist.challenge.renaldi.entity.User;
+import ist.challenge.renaldi.exception.user.UserAlreadyExistException;
 import ist.challenge.renaldi.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,6 +21,8 @@ public class UserService {
     private UserRepository repository;
     
     public void registerUser(User newUser){
+        if(repository.existsByUsername(newUser.getUsername()))
+            throw UserAlreadyExistException.defaultException();
         repository.save(newUser);
     }
     
