d037a6de80066ef5b6bec7a110c36906207ee555
Cardinal Alpha <renaldi96.aldi@gmail.com>
2023-11-17 15:12:52 +0700

    Add mapping for existing data

--- src/main/java/ist/challenge/renaldi/mapper/UserMapper.java
+++ src/main/java/ist/challenge/renaldi/mapper/UserMapper.java
@@ -8,6 +8,8 @@ import ist.challenge.renaldi.entity.User;
 import ist.challenge.renaldi.pojo.UserDataInput;
 import ist.challenge.renaldi.pojo.UserDataOutput;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 /**
  *
@@ -19,5 +21,8 @@ public interface UserMapper {
     User mapInput(UserDataInput registration);
     
     UserDataOutput mapOutput(User user);
+    
+    @Mapping(target = "id", ignore = true)
+    void updateExisting(User src, @MappingTarget User dest);
     
 }
